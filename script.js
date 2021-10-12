43beb492cf505347cae4663a383fd0e57dfbcde5
Ravenclaw-OIer <lilywhite2005@outlook.com>
2021-10-12 14:11:31 +0800

    updated

--- script.js
+++ script.js
@@ -81,23 +81,24 @@ let currentSlide = 0;
 
 const questions = [
   {
-    question: "Who invented JavaScript?",
+    question: "小勇尚年幼，家人因为新冠肺炎疫情被隔离，谁来照顾他？",
     answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich"
+      a: "周春",
+      b: "居民委员会",
+      c: "孤儿院"
     },
-    correctAnswer: "c",
-    analysis: "B. Eich 发明了 JavaScript"
+    correctAnswer: "b",
+    analysis: "《民法典》第三十四条规定，因发生突发事件等紧急情况，监护人暂时无法履行监护职责，被监护人的生活处于无人照料状态的，被监护人住所地的居民委员会、村民委员会或者民政部门应当为被监护人安排必要的临时生活照料措施。"
   },
   {
-    question: "Which one of these is a JavaScript package manager?",
+    question: "你在京东上买了一本《教材完全解读》，但送来后发现只剩下了半本，卖家说寄出之前书是完整的，那么谁承担书坏的责任",
     answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm"
+      a: "快递公司",
+      b: "卖家",
+      c: "你"
     },
-    correctAnswer: "c"
+    correctAnswer: "b",
+    analysis: "《民法典》第五百一十二条规定，通过互联网等信息网络订立的电子合同的标的为交付商品并采用快递物流方式交付的，收货人的签收时间为交付时间。第六百零四条规定，标的物毁损、灭失的风险，在标的物交付之前由出卖人承担，交付之后由买受人承担，但是法律另有规定或者当事人另有约定的除外。"
   },
   {
     question: "Which tool can you use to ensure code quality?",
